871e3b55de96c65e881eb8740f8c399f4a46131e
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2020-02-12 19:36:02 +0000

    Improve styling of plot: inset plots, level annotations & more

--- _includes/generate-std-name-totals.py
+++ _includes/generate-std-name-totals.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator, ScalarFormatter
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 import os
 import pprint
 import re
+
 
 # Run from root repo dir (or if from 'includes' dir, add initial ".."):
 STD_NAME_ROOT_DIR_RELATIVE_PATH = os.path.join("Data", "cf-standard-names")
@@ -55,8 +59,9 @@ def get_all_std_names_per_version(root_dir, return_names=False):
             try:
                 date = get_from_file(XML_LAST_MODIFIED_PATTERN, filename)[0]
             except:
-                #raise Exception("Can't find date")
-                date = "2000-01-01"  # USE AS PLOTTABLE PLACEHOLDER FOR NOW
+                # No timestamp on v1, so assume from v1.0 of CF, Oct 2003 (see
+                # http://cfconventions.org/faq.html#when_started)
+                date = "2003-10-01"
             totals[version] = {"total": total, "date": date}
         else:
             names[version] = names_in_version
@@ -74,11 +79,10 @@ def calculate_difference_totals(totals_data):
         else:
             try:
                 previous_ver_data = totals_data[ver - 1]
-            except KeyError:  # rarely version is missing, but not >1 together
+            except KeyError:  # account for case of v39 (v38 was skipped)
                 previous_ver_data = totals_data[ver - 2]
             totals_with_diff_data[ver].update(
                 {"diff": data["total"] - previous_ver_data["total"]})
-    print(totals_with_diff_data)
     return totals_with_diff_data
 
 
@@ -118,9 +122,11 @@ def convert_date_str(date_str):
 
 
 def make_raw_and_difference_plot(totals_figures, by_date=True):
-    fig, (ax1, ax2) = plt.subplots(2)
+    LINEWIDTH = 3
+    
     totals_figures = pre_process(totals_figures)
     totals_figures = calculate_difference_totals(totals_figures)
+    pprint.pprint(totals_figures)  ### DEBUG
 
     totals = {}
     diffs = {}
@@ -132,11 +138,135 @@ def make_raw_and_difference_plot(totals_figures, by_date=True):
             totals[ver] = data["total"]
             diffs[ver] = data["diff"]
 
+    pprint.pprint(totals)  ### DEBUG
+
     sorted_totals = sorted(totals.items())
     sorted_diffs = sorted(diffs.items())
-    ax1.plot(*zip(*sorted_totals), 'ko-')
-    ax2.plot(*zip(*sorted_diffs), 'r.-')
 
+    plt.rcParams.update({'font.size': 12})
+    #fig, (ax1, ax2) = plt.subplots(2)
+    fig, ax1 = plt.subplots()
+    ax1.set_title(
+        'Number of standard names in the CF conventions table by date',
+        fontsize=18
+    )
+    # Remove horizontal space between axes
+    fig.subplots_adjust(hspace=0)
+    ax1.set_xlabel(
+        "Date of table version (marked at Jan 1st)",
+        fontsize=14
+    )
+    ax1.tick_params(axis='x', which='minor')
+    ax1.set_ylabel(
+        "Total number",
+        fontsize=14
+    )
+    ax1.xaxis.set_minor_locator(AutoMinorLocator(2))
+    ax2 = ax1.twinx()
+    ax2.set_ylabel(
+        "Difference in total number \nrelative to previous version",
+        fontsize=14,
+        rotation=270,
+        labelpad=35
+    )
+    ax2.yaxis.set_minor_locator(AutoMinorLocator(2))
+
+
+    axins1 = inset_axes(ax1, width="70%", height="33%", loc="upper left")
+    axins1.yaxis.tick_right()
+
+    axins2 = inset_axes(
+        ax1, width="50%", height="30%", loc="lower right", borderpad=3)
+    
+    ax1.step(*zip(*sorted_totals), where='post',
+             linestyle='-', color='crimson', alpha=0.4,
+             linewidth=LINEWIDTH, zorder=2)
+
+    st, = ax1.plot(
+        *zip(*sorted_totals),
+        marker=None, linestyle='dashed', color='crimson',
+        linewidth=LINEWIDTH)
+    ax1.yaxis.label.set_color(st.get_color())
+    ax1.set_ylim(0, 4500)
+
+    axins2.step(*zip(*sorted_totals), where='post',
+             linestyle='-', color='crimson', alpha=0.4, linewidth=LINEWIDTH)
+
+    axins2.plot(
+        *zip(*sorted_totals),
+        marker=None, linestyle='dashed', color='crimson',
+        linewidth=LINEWIDTH)
+    axins2.set_xlim(datetime(2019, 1, 1, 0, 0), datetime.now())
+    axins2.set_ylim(4300, 4420)
+    #axins2.set_yticklabels([])
+    #axins2.set_xticklabels([])
+    mark_inset(ax1, axins2, loc1=2, loc2=4, fc="none", ec="0.5")
+
+    ax2.set_zorder(3)
+    dt = ax2.stem(
+        *zip(*sorted_diffs), use_line_collection=True, bottom=0)
+
+    # Version label annotation:
+    for ver, data in totals_figures.items():
+        if ver % 5 == 0:  # annotate version every 5 versions
+            x = convert_date_str(data["date"])
+            y_diff = data["diff"]
+            y_total = data["total"]
+            ax1.annotate(
+                str(ver), xy=(x, y_total), xytext=(x, y_total-500),
+                color='darkgoldenrod', alpha=0.75,
+                arrowprops=dict(
+                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=0.05,
+                    shrinkB=0.05,
+                    alpha=0.75)
+            )
+            ax2.annotate(
+                "", xy=(x, y_diff), xytext=(x, y_diff+100),
+                color='darkgoldenrod',
+                arrowprops=dict(
+                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=1.0,
+                    shrinkB=1.0,
+                    alpha=0.75)
+            )
+            axins1.annotate(
+                "", xy=(x, y_diff), xytext=(x, y_diff+100),
+                color='darkgoldenrod',
+                arrowprops=dict(
+                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=1.0,
+                    shrinkB=1.0,
+                    alpha=0.75)
+            )
+            axins2.annotate(
+                str(ver), xy=(x, y_total), xytext=(x, y_total-500),
+                color='darkgoldenrod', alpha=0.75,
+                arrowprops=dict(
+                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=1.0,
+                    shrinkB=1.0,
+                    alpha=0.75)
+            )
+
+
+
+    ax2.yaxis.label.set_color('C0')  # default matplotlib blue now
+
+    ax1.tick_params(axis='y', colors=st.get_color())
+    ax2.tick_params(axis='y', colors='C0')
+
+    
+    ax2.set_ylim(1, 1400)
+    #ax2.plot(*zip(*sorted_diffs), 'r.-')
+    axins1.plot(*zip(*sorted_diffs), marker='o', linestyle='None')  # needs string!
+    axins1.set_ylim(1, 1500)
+    axins1.set_yticks([1, 10, 100, 1000])
+    axins1.set_yscale('log')
+    axins1.yaxis.set_major_formatter(ScalarFormatter())
+    axins1.yaxis.label.set_color(st.get_color())
+
+
+    axins1.set_xlim(datetime(2006, 9, 26, 0, 0), datetime.now())
+
+    axins1.plot()  # fix zorder, when can't use on stem?
+    ###fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
 
 
@@ -186,14 +316,14 @@ def print_version_comparison(newer_version, older_version):
 
 
 # Inspect & print name differences as list to pass to online vis tool:
-print_version_comparison(12, 11)
-print_version_comparison(49, 48)
+###print_version_comparison(12, 11)
+###print_version_comparison(49, 48)
 
 totals_data = get_all_std_names_per_version(STD_NAME_ROOT_DIR_RELATIVE_PATH)
 
 # Raw/crude table of totals per version:
-pprint.pprint(totals_data)
+###pprint.pprint(totals_data)
 
 # Nicer in a plot:
-make_plot_against_versions(totals_data)
+###make_plot_against_versions(totals_data)
 make_plot_against_dates(totals_data)
