64802212253c43a066f9c41f5a271ddadb9d930d
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2024-09-19 21:30:43 +0100

    Apply black and isort linting tools to original vis script

--- vis/snames-totals-and-wordclouds.py
+++ vis/snames-totals-and-wordclouds.py
@@ -1,35 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-import matplotlib.dates as dates
-import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator, ScalarFormatter
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 import os
 import pprint
 import re
-from wordcloud import WordCloud
+from datetime import datetime
 
+import matplotlib.dates as dates
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator, ScalarFormatter
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
+from wordcloud import WordCloud
 
 # Run from root repo dir (or if from 'includes' dir, add initial ".."):
 STD_NAME_ROOT_DIR_RELATIVE_PATH = os.path.join("Data", "cf-standard-names")
 
 
-XML_STD_NAME_TAG_PATTERN = re.compile(r'<entry id=\"(.+)\">')
-XML_LAST_MODIFIED_PATTERN = re.compile(
-    r'<last_modified>(.+)T(.+)</last_modified>')
+XML_STD_NAME_TAG_PATTERN = re.compile(r"<entry id=\"(.+)\">")
+XML_LAST_MODIFIED_PATTERN = re.compile(r"<last_modified>(.+)T(.+)</last_modified>")
 
 
 def get_from_file(pattern, std_name_xml_filename):
     extracted_data = []
-    with open(std_name_xml_filename, 'rt') as std_name_xml_data:    
+    with open(std_name_xml_filename, "rt") as std_name_xml_data:
         for line in std_name_xml_data:
             full_pattern_result = pattern.search(line)
             if full_pattern_result:
-                extracted_data.append(
-                    full_pattern_result.group(1).rstrip('\n'))
+                extracted_data.append(full_pattern_result.group(1).rstrip("\n"))
     return extracted_data
 
 
@@ -38,7 +35,7 @@ def get_total_per_version(std_names_list):
 
 
 def extract_xml_by_version_from_std_name_dir(std_name_dir):
-    """ Walk 'cf-convention.github.io/Data/cf-standard-names/' dir for XML. """
+    """Walk 'cf-convention.github.io/Data/cf-standard-names/' dir for XML."""
     all_xml_file_paths = {}
     for dir_path, _, file_list in os.walk(std_name_dir):
         if dir_path.endswith("src"):
@@ -83,15 +80,17 @@ def calculate_difference_totals(totals_data):
             except KeyError:  # account for case of v39 (v38 was skipped)
                 previous_ver_data = totals_data[ver - 2]
             totals_with_diff_data[ver].update(
-                {"diff": data["total"] - previous_ver_data["total"]})
+                {"diff": data["total"] - previous_ver_data["total"]}
+            )
     return totals_with_diff_data
 
 
 def process_current(totals_figures):
     # Convert versions to integers for plotting:
     current_data = totals_figures["current"]
-    totals_figures = {int(ver): data for ver, data in
-                      totals_figures.items() if ver != "current"}
+    totals_figures = {
+        int(ver): data for ver, data in totals_figures.items() if ver != "current"
+    }
     # Convert current to assumed latest version, for plotting version as int:
     highest_vesion = max(totals_figures.keys())
     assume_current_version = highest_vesion + 1
@@ -100,13 +99,12 @@ def process_current(totals_figures):
 
 
 def pre_process(all_totals_data):
-    """ Any processing on the raw data required pre-plot. """
+    """Any processing on the raw data required pre-plot."""
     # Convert 'current' to latest version number (assumed)
     all_totals_data = process_current(all_totals_data)
 
     # Convert version strings to integers so they become plotable
-    all_totals_data = {int(ver): data for ver, data in
-                       all_totals_data.items()}
+    all_totals_data = {int(ver): data for ver, data in all_totals_data.items()}
 
     # Version 23 date issue, remove extra character that shouldn't be there:
     date_v23 = all_totals_data[23]["date"]
@@ -124,7 +122,7 @@ def convert_date_str(date_str):
 
 def make_raw_and_difference_plot(totals_figures, by_date=True):
     LINEWIDTH = 3
-    
+
     totals_figures = pre_process(totals_figures)
     totals_figures = calculate_difference_totals(totals_figures)
     pprint.pprint(totals_figures)  ### DEBUG
@@ -144,68 +142,76 @@ def make_raw_and_difference_plot(totals_figures, by_date=True):
     sorted_totals = sorted(totals.items())
     sorted_diffs = sorted(diffs.items())
 
-    plt.rcParams.update({'font.size': 12})
-    #fig, (ax1, ax2) = plt.subplots(2)
+    plt.rcParams.update({"font.size": 12})
+    # fig, (ax1, ax2) = plt.subplots(2)
     fig, ax1 = plt.subplots()
     ax1.set_title(
-        'Number of standard names in the CF conventions table by date',
-        fontsize=18
+        "Number of standard names in the CF conventions table by date", fontsize=18
     )
     # Remove horizontal space between axes
     fig.subplots_adjust(hspace=0)
-    ax1.set_xlabel(
-        "Date of table version (marked at Jan 1st)",
-        fontsize=14
-    )
-    ax1.tick_params(axis='x', which='minor')
-    ax1.set_ylabel(
-        "Total number",
-        fontsize=14
-    )
+    ax1.set_xlabel("Date of table version (marked at Jan 1st)", fontsize=14)
+    ax1.tick_params(axis="x", which="minor")
+    ax1.set_ylabel("Total number", fontsize=14)
     ax1.xaxis.set_minor_locator(AutoMinorLocator(2))
     ax2 = ax1.twinx()
     ax2.set_ylabel(
         "Difference in total number \nrelative to previous version",
         fontsize=14,
         rotation=270,
-        labelpad=35
+        labelpad=35,
     )
     ax2.yaxis.set_minor_locator(AutoMinorLocator(2))
-
 
     axins1 = inset_axes(ax1, width="70%", height="33%", loc="upper left")
     axins1.yaxis.tick_right()
 
-    axins2 = inset_axes(
-        ax1, width="50%", height="30%", loc="lower right", borderpad=3)
-    
-    ax1.step(*zip(*sorted_totals), where='post',
-             linestyle='-', color='crimson', alpha=0.4,
-             linewidth=LINEWIDTH, zorder=2)
+    axins2 = inset_axes(ax1, width="50%", height="30%", loc="lower right", borderpad=3)
 
-    st, = ax1.plot(
+    ax1.step(
         *zip(*sorted_totals),
-        marker=None, linestyle='dashed', color='crimson',
-        linewidth=LINEWIDTH)
+        where="post",
+        linestyle="-",
+        color="crimson",
+        alpha=0.4,
+        linewidth=LINEWIDTH,
+        zorder=2
+    )
+
+    (st,) = ax1.plot(
+        *zip(*sorted_totals),
+        marker=None,
+        linestyle="dashed",
+        color="crimson",
+        linewidth=LINEWIDTH
+    )
     ax1.yaxis.label.set_color(st.get_color())
     ax1.set_ylim(0, 4500)
 
-    axins2.step(*zip(*sorted_totals), where='post',
-             linestyle='-', color='crimson', alpha=0.4, linewidth=LINEWIDTH)
+    axins2.step(
+        *zip(*sorted_totals),
+        where="post",
+        linestyle="-",
+        color="crimson",
+        alpha=0.4,
+        linewidth=LINEWIDTH
+    )
 
     axins2.plot(
         *zip(*sorted_totals),
-        marker=None, linestyle='dashed', color='crimson',
-        linewidth=LINEWIDTH)
+        marker=None,
+        linestyle="dashed",
+        color="crimson",
+        linewidth=LINEWIDTH
+    )
     axins2.set_xlim(datetime(2019, 1, 1, 0, 0), datetime.now())
     axins2.set_ylim(4300, 4420)
-    #axins2.set_yticklabels([])
-    #axins2.set_xticklabels([])
+    # axins2.set_yticklabels([])
+    # axins2.set_xticklabels([])
     mark_inset(ax1, axins2, loc1=2, loc2=4, fc="none", ec="0.5")
 
     ax2.set_zorder(3)
-    dt = ax2.stem(
-        *zip(*sorted_diffs), use_line_collection=True, bottom=0)
+    dt = ax2.stem(*zip(*sorted_diffs), use_line_collection=True, bottom=0)
 
     # Version label annotation:
     for ver, data in totals_figures.items():
@@ -214,55 +220,73 @@ def make_raw_and_difference_plot(totals_figures, by_date=True):
             y_diff = data["diff"]
             y_total = data["total"]
             ax1.annotate(
-                str(ver), xy=(x, y_total), xytext=(x, y_total-500),
-                color='darkgoldenrod', alpha=0.75,
+                str(ver),
+                xy=(x, y_total),
+                xytext=(x, y_total - 500),
+                color="darkgoldenrod",
+                alpha=0.75,
                 arrowprops=dict(
-                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=0.05,
+                    fc="darkgoldenrod",
+                    ec="darkgoldenrod",
+                    shrinkA=0.05,
                     shrinkB=0.05,
-                    alpha=0.75)
+                    alpha=0.75,
+                ),
             )
             ax2.annotate(
-                "", xy=(x, y_diff), xytext=(x, y_diff+100),
-                color='darkgoldenrod',
+                "",
+                xy=(x, y_diff),
+                xytext=(x, y_diff + 100),
+                color="darkgoldenrod",
                 arrowprops=dict(
-                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=1.0,
+                    fc="darkgoldenrod",
+                    ec="darkgoldenrod",
+                    shrinkA=1.0,
                     shrinkB=1.0,
-                    alpha=0.75)
+                    alpha=0.75,
+                ),
             )
             axins1.annotate(
-                "", xy=(x, y_diff), xytext=(x, y_diff+100),
-                color='darkgoldenrod',
+                "",
+                xy=(x, y_diff),
+                xytext=(x, y_diff + 100),
+                color="darkgoldenrod",
                 arrowprops=dict(
-                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=1.0,
+                    fc="darkgoldenrod",
+                    ec="darkgoldenrod",
+                    shrinkA=1.0,
                     shrinkB=1.0,
-                    alpha=0.75)
+                    alpha=0.75,
+                ),
             )
             axins2.annotate(
-                str(ver), xy=(x, y_total), xytext=(x, y_total-500),
-                color='darkgoldenrod', alpha=0.75,
+                str(ver),
+                xy=(x, y_total),
+                xytext=(x, y_total - 500),
+                color="darkgoldenrod",
+                alpha=0.75,
                 arrowprops=dict(
-                    fc='darkgoldenrod', ec='darkgoldenrod', shrinkA=1.0,
+                    fc="darkgoldenrod",
+                    ec="darkgoldenrod",
+                    shrinkA=1.0,
                     shrinkB=1.0,
-                    alpha=0.75)
+                    alpha=0.75,
+                ),
             )
 
+    ax2.yaxis.label.set_color("C0")  # default matplotlib blue now
 
+    ax1.tick_params(axis="y", colors=st.get_color())
+    ax2.tick_params(axis="y", colors="C0")
 
-    ax2.yaxis.label.set_color('C0')  # default matplotlib blue now
-
-    ax1.tick_params(axis='y', colors=st.get_color())
-    ax2.tick_params(axis='y', colors='C0')
-
-    
     ax2.set_ylim(1, 1400)
-    #ax2.plot(*zip(*sorted_diffs), 'r.-')
-    axins1.plot(*zip(*sorted_diffs), marker='o', linestyle='None')  # needs string!
+    # ax2.plot(*zip(*sorted_diffs), 'r.-')
+    axins1.plot(*zip(*sorted_diffs), marker="o", linestyle="None")  # needs string!
     axins1.set_ylim(1, 1500)
     axins1.set_yticks([1, 10, 100, 1000])
-    axins1.set_yscale('log')
+    axins1.set_yscale("log")
     axins1.yaxis.set_major_formatter(ScalarFormatter())
     axins1.yaxis.label.set_color(st.get_color())
-
 
     axins1.set_xlim(datetime(2006, 9, 26, 0, 0), datetime.now())
 
@@ -279,8 +303,9 @@ def make_plot_against_versions(totals_figures):
     make_raw_and_difference_plot(totals_figures, by_date=False)
 
 
-def get_new_names(all_std_names_per_version, newer_version, older_version,
-                  print_on=False):
+def get_new_names(
+    all_std_names_per_version, newer_version, older_version, print_on=False
+):
     newer_set = set(all_std_names_per_version[str(newer_version)])
     older_set = set(all_std_names_per_version[str(older_version)])
     difference = list(newer_set.difference(older_set))
@@ -307,11 +332,12 @@ def convert_underscored_phrase_to_words(all_names_list):
 def print_version_comparison(newer_version, older_version):
     new_names = get_new_names(
         get_all_std_names_per_version(
-            STD_NAME_ROOT_DIR_RELATIVE_PATH, return_names=True),
-        newer_version, older_version
+            STD_NAME_ROOT_DIR_RELATIVE_PATH, return_names=True
+        ),
+        newer_version,
+        older_version,
     )
-    print("\n\n\n", "-" * 15, "New to v%s" % str(newer_version),
-          "-" * 15, "\n\n\n")
+    print("\n\n\n", "-" * 15, "New to v%s" % str(newer_version), "-" * 15, "\n\n\n")
     names_spaced = convert_underscored_phrase_to_words(new_names)
     for sname in names_spaced:
         print(sname)
@@ -319,9 +345,9 @@ def print_version_comparison(newer_version, older_version):
 
 
 def make_wordcloud(text):
-    """ Create wordcloud for version differences in standard names. """
-    wordcloud = WordCloud(background_color='white').generate(text)
-    plt.imshow(wordcloud, interpolation='bilinear')
+    """Create wordcloud for version differences in standard names."""
+    wordcloud = WordCloud(background_color="white").generate(text)
+    plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
     plt.show()
 
